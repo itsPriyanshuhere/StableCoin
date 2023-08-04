55a6b280d532e797ef27a4cd1c4cbc54b83ff068
itsPriyanshuhere <priyanshupandey256@gmail.com>
2023-08-04 12:18:04 +0530

    Updated.

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -11,7 +11,6 @@ contract HelperConfig is Script {
     uint8 public constant DECIMALS = 8;
     int256 public constant ETH_USD_PRICE = 2000e8;
     int256 public constant BTC_USD_PRICE = 1000e8;
-    uint256 public DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     struct NetworkConfig {
         address wethUsdPriceFeed;
@@ -20,6 +19,8 @@ contract HelperConfig is Script {
         address wbtc;
         uint256 deployerKey;
     }
+
+    uint256 public DEFAULT_ANVIL_PRIVATE_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
 
     constructor() {
         if (block.chainid == 11155111) {
@@ -50,12 +51,13 @@ contract HelperConfig is Script {
             DECIMALS,
             ETH_USD_PRICE
         );
-        ERC20Mock wethMock = new ERC20Mock();
+        ERC20Mock wethMock = new ERC20Mock("WETH", "WETH", msg.sender, 1000e8);
+
         MockV3Aggregator btcUsdPriceFeed = new MockV3Aggregator(
             DECIMALS,
             BTC_USD_PRICE
         );
-        ERC20Mock wbtcMock = new ERC20Mock();
+        ERC20Mock wbtcMock = new ERC20Mock("WBTC", "WBTC", msg.sender, 1000e8);
         vm.stopBroadcast();
 
         anvilNetworkConfig = NetworkConfig({
@@ -63,7 +65,7 @@ contract HelperConfig is Script {
             weth: address(wethMock),
             wbtcUsdPriceFeed: address(btcUsdPriceFeed),
             wbtc: address(wbtcMock),
-            deployerKey: DEFAULT_ANVIL_KEY
+            deployerKey: DEFAULT_ANVIL_PRIVATE_KEY
         });
     }
 }
